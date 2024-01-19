99868482bec0f470233c9cb04149f72d96965313
Oleg Proskurin <proskur@altlinux.org>
2024-01-19 14:40:41 +0300

    define constructor

--- lispmessage.cpp
+++ lispmessage.cpp
@@ -2,9 +2,14 @@
 
 LispMessage::LispMessage() {}
 
+LispMessage::LispMessage(const std::string& act,
+                const std::string& obj,
+                const std::unordered_map<std::string,std::string>& prms):
+                action(act), objects(obj), params(prms){}
+
 std::ostream& operator  << (std::ostream& os,const LispMessage& mes){
     os << "Action: " << mes.action <<std::endl
-              << "Objects: " << mes.objects;
+              << "Objects: " << mes.objects << std::endl;
     for (auto it = mes.params.cbegin(); it != mes.params.end(); ++it){
         os << it->first << " : " << it->second << std::endl;
     }
--- lispmessage.hpp
+++ lispmessage.hpp
@@ -2,17 +2,20 @@
 #define LISPMESSAGE_HPP
 
 #include <string>
-#include <map>
+#include <unordered_map>
 #include <iostream>
 
 class LispMessage
 {
 public:
     LispMessage();
+    LispMessage(const std::string& act,
+                const std::string& obj,
+                const std::unordered_map<std::string,std::string>& prms);
 
     std::string action;
     std::string objects;
-    std::map<std::string,std::string> params;
+    std::unordered_map<std::string,std::string> params;
 };
 
 std::ostream& operator  << (std::ostream& os,const LispMessage& mes);
