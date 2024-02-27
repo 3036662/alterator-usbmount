061051bb522461630c3ff14d2161425b4b8072df
Oleg Proskurin <proskur@altlinux.org>
2024-02-27 17:38:00 +0300

    remove whitespaces

--- backend/serializible_for_lisp.hpp
+++ backend/serializible_for_lisp.hpp
@@ -13,7 +13,6 @@ public:
 
 template <> class SerializableForLisp<vecPairs> {
   vecPairs vec;
-
 public:
   SerializableForLisp(vecPairs &&vec_) : vec{vec_} {};
   vecPairs SerializeForLisp() const { return vec; }
