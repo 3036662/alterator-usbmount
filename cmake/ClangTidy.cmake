6206d6aac9d4b1c33ae2715da5fb430c4d23a035
Oleg Proskurin <proskur@altlinux.org>
2024-02-28 09:46:39 +0300

    fix clang-tidy path

--- cmake/ClangTidy.cmake
+++ cmake/ClangTidy.cmake
@@ -2,5 +2,5 @@ function(AddClangTidy target)
     find_program(CLANG-TIDY_PATH clang-tidy REQUIRED)  
     set_target_properties(${target}    
         PROPERTIES CXX_CLANG_TIDY    
-        "${CLANG-TIDY_PATH};")
+        "${CLANG-TIDY_PATH}")
 endfunction()
