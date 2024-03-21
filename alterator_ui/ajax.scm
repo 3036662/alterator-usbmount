c62c258d4267533e6693116fafd3f295cc1f4dc2
Oleg Proskurin <proskur@altlinux.org>
2024-03-21 13:28:15 +0300

    update error message text

--- alterator_ui/ajax.scm
+++ alterator_ui/ajax.scm
@@ -148,7 +148,7 @@
             (if   
                 (equal? "OK" (woo-get-option status 'status))
                 (update_after_rulles_applied)
-                (woo-error (_ "Error while trying to unblock selected device. You can unblock it using hash or interface."))
+                (woo-error (_ "Error while trying to unblock selected device. You can unblock it with crearing hash rule or interface rule."))
             )
         ); let
     ) ; endif
