e64b0cbb06681c49322bcdd88a16091c76878a07
Oleg Proskurin <proskur@altlinux.org>
2024-04-11 15:08:46 +0300

    add javadoc for mount_points.hpp

--- daemon/dal/mount_points.hpp
+++ daemon/dal/mount_points.hpp
@@ -1,18 +1,54 @@
 #pragma once
+#include "dto.hpp"
 #include "table.hpp"
+#include <cstdint>
+#include <optional>
 
 namespace usbmount::dal {
 
 class Mountpoints : public Table {
 public:
+  /**
+   * @brief Construct a new Mountpoints object
+   * @param path Path to a file to store data
+   * @throws runtime_error
+   */
   Mountpoints(const std::string &path);
 
   // CRUD
+
+  /**
+   * @brief  Create a new entry for a mountpoint in the local storage
+   * @throws bad_cast , runtime_error
+   */
   void Create(const Dto &) override;
+
+  /**
+   * @brief Get entry by index
+   * @return const MountEntry&
+   * @throws std::invalid_argument (wrong index), out_of_range by std::map
+   */
   const MountEntry &Read(uint64_t) const override;
+
+  /**
+   * @brief Update entry by index
+   * @throws bad_cast, invalid_argument (index)
+   */
   void Update(uint64_t, const Dto &) override;
 
+  /**
+   * @brief Find entry
+   *
+   * @param entry
+   * @return std::optional<uint64_t> index or empry if nothing was found
+   */
+  std::optional<uint64_t> Find(const MountEntry &entry) const noexcept;
+
 private:
+  /**
+   * @brief Read raw_json_ and fill the fields with data
+   * @throws runtime_error, system_error (json  parser)
+   */
   void DataFromRawJson() override;
 };
 
