67c1b97cdd3fe2f689a7f8c3d97a88bf048c84c5
Oleg Proskurin <proskur@altlinux.org>
2024-04-26 14:35:47 +0300

    add russian translation

---
+++ alterator_usbmount/usbmount_ui/update_po.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+configd-xgettext index.html | xgettext -L Lisp --keyword=_ -o alterator-usbmount.pot -- -
+xgettext -L Lisp  --join-existing --keyword=_ -o alterator-usbmount.pot ajax.scm
+cat USBMount.desktop |  sed -n -e "s,^Name[[:space:]]*=[[:space:]]*\(.*\),(_ \"\1\"),p" -e "s,^Comment[[:space:]]*=[[:space:]]*\(.*\),(_ \"\1\"),p" | xgettext -L Lisp --join-existing --keyword=_ -o alterator-usbmount.pot -- -
+msgmerge alterator-usbmount.po alterator-usbmount.pot -o alterator-usbmount.po
