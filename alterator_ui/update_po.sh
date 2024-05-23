2d5bcc8731ee2cb2cce092ebfd94da7ceba768e7
Oleg Proskurin <proskur@altlinux.org>
2024-05-23 10:12:05 +0300

    move alterator-usbmount to separate git repo

--- alterator_ui/update_po.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-configd-xgettext index.html | xgettext -L Lisp --keyword=_  --from-code utf-8 -o alterator-usbguard.pot -- -
-xgettext -L Lisp  --join-existing --keyword=_ -o alterator-usbguard.pot ajax.scm
-cat USBGuard.desktop |  sed -n -e "s,^Name[[:space:]]*=[[:space:]]*\(.*\),(_ \"\1\"),p" -e "s,^Comment[[:space:]]*=[[:space:]]*\(.*\),(_ \"\1\"),p" | xgettext -L Lisp --join-existing --keyword=_ -o alterator-usbguard.pot -- -
-msgmerge alterator-usbguard.po alterator-usbguard.pot -o alterator-usbguard.po
