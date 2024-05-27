427ce94e86aa2ce16d8a861b74e1bdaa8ff75bd7
Oleg Proskurin <proskur@altlinux.org>
2024-05-27 11:58:41 +0300

    add license headers to sources

--- daemon/dbus_methods.hpp
+++ daemon/dbus_methods.hpp
@@ -1,3 +1,23 @@
+/* File: dbus_methods.hpp
+
+  Copyright (C)   2024
+  Author: Oleg Proskurin, <proskurinov@basealt.ru>
+
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this program; if not, see <https://www.gnu.org/licenses/>.
+
+*/
+
 #pragma once
 #include "dal/local_storage.hpp"
 #include "udev_monitor.hpp"
